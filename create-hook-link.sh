c4e22169929e3b3d42cee10d497fe9ab75c560aa
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-19 11:03:01 +0800

    +: * —— 新增钩子和脚本

---
+++ create-hook-link.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+: << !
+Name: create-hook-link.sh
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-09-18 13:37:04
+
+Description: 创建git钩子
+
+Attentions:
+-
+
+Depends:
+-
+!
+
+hook_work_dir=".git/hooks"
+mkdir -p "$hook_work_dir"
+
+hook_base_dir="hooks"
+path=$(ls "$hook_base_dir")
+for file in $path; do
+  ln -sf ../../$hook_base_dir/"$file" "$hook_work_dir/$file"
+done
